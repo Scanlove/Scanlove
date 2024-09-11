f8f9a3d6906b6fb26db309daf906eb52e8d73f49
Scanlove <Scanlove665@gmail.com>
2024-09-11 16:32:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let progress = 0;
 let loader = document.getElementById('loader');
 let content = document.getElementById('content');
 let progressText = document.getElementById('progress');
+let batteryLevel = document.getElementById('battery-level');
 
 function simulateLoading() {
     if (progress < 100) {
         progress++;
         progressText.innerText = progress;
+        batteryLevel.style.width = progress + '%'; // Incrementa el ancho de la "batería"
         setTimeout(simulateLoading, 50); // Velocidad de la animación
     } else {
         loader.style.display = 'none';
